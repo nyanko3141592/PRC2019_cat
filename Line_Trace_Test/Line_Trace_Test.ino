f2201ce265580b8373744ef04e48d7d7824d6f30
NanokiTakahashi <takahashinaoki521@gmail.com>
2020-02-02 11:30:10 +0900

    多かったelseを削除

--- Line_Trace_Test/Line_Trace_Test.ino
+++ Line_Trace_Test/Line_Trace_Test.ino
@@ -1,43 +1,62 @@
-#include"parameters.h"
+#include "parameters.h"
 
-void setup() {
+void setup()
+{
   pinMode(RF, OUTPUT);
   pinMode(RB, OUTPUT);
   pinMode(LF, OUTPUT);
   pinMode(LB, OUTPUT);
-  Serial.begin(9600);//PC (Debug)
+  Serial.begin(9600); //PC (Debug)
 }
 
-void loop() {
-  if (analogRead(R1) < DR1 && analogRead(L1) < DL1){
-    Move(MSL,MSR);
+void loop()
+{
+  // B???B
+  if (analogRead(R1) < DR1 && analogRead(L1) < DL1)
+  {
+    // 0.5s forward
+    Serial.println("B???B");
+    Move(MSL, MSR);
     delay(500);
-    if(analogRead(CT) > DCT){ //dead end (U-turn)
-      Move(MSL,-MSR);
+    // B?B?B
+    if (analogRead(CT) > DCT)
+    { //dead end (U-turn)
+      Serial.println("B?B?B");
+      Move(MSL, -MSR);
       delay(2500);
     }
   }
-  if (analogRead(R1) < DR1) { //turn right
+  // ????B
+  if (analogRead(R1) < DR1)
+  { //turn right
+    Serial.println("????B");
     Move(MSL, 0);
     delayMicroseconds(10);
   }
-  else if (analogRead(L1) < DL1) { //left sensor detected
-    if (analogRead(L2) < DL2) { //left outer sensor detected
-        Move(MSL,MSR);
-        delay(200);
-        if(analogRead(R2) > DR2){//spin left
-          Move(-MSL, MSR);
-          delay(1250);
-        }        
+  // B???W
+  else if (analogRead(L1) < DL1)
+  {
+    Serial.println("W???B");
+    // BB??W
+    if (analogRead(L2) < DL2)
+    { //left outer sensor detected
+      Serial.println("BB??W");
+      Move(MSL, MSR);
+      delay(200);
+      // BB?WW
+      if (analogRead(R2) > DR2)
+      { //spin left
+        Serial.println("BB?WW");
+        Move(-MSL, MSR);
+        delay(1250);
       }
     }
-    else { //turn left
-      Move(0, MSR);
-      delayMicroseconds(10);
-    }
   }
-  else { //fwdstr
-    Move(MSL, MSR);    
-    delayMicroseconds(50);
+  // ????W
+  else
+  { //turn left
+    Serial.println("????W");
+    Move(MSL, MSR);
+    delayMicroseconds(10);
   }
 }
