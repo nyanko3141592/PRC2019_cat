90b3b2296ed737ac29cecb9656c8628a5277692b
NanokiTakahashi <takahashinaoki521@gmail.com>
2020-02-02 15:14:08 +0900

    書き方変えてみた

--- Line_Trace_Test/Line_Trace_Test.ino
+++ Line_Trace_Test/Line_Trace_Test.ino
@@ -21,62 +21,132 @@ const int MSL = 100;
 const int MSR = 100;
 //↑motor speed
 
+int location_info = 0;
+bool flag = true ;
+
 void Move (int Lp, int Rp);
 
 void setup() {
-  pinMode(RF, OUTPUT);
-  pinMode(RB, OUTPUT);
-  pinMode(LF, OUTPUT);
-  pinMode(LB, OUTPUT);
+    pinMode(RF, OUTPUT);
+    pinMode(RB, OUTPUT);
+    pinMode(LF, OUTPUT);
+    pinMode(LB, OUTPUT);
+    Serial.begin(9600); //PC (Debug)    
 }
 
 void loop() {
-  if (analogRead(R1) < DR1) {
+    int sum_black = 5; 
+    const bool R1_black = analogRead(R1) < DR1;
+    const bool R2_black = analogRead(R2) < DR2;
+    const bool CT_black = analogRead(CT) < DCT;
+    const bool L1_black = analogRead(L2) < DL2;
+    const bool L2_black = analogRead(L1) < DL1;
+    sum_black = R1_black ? sum_black : sum_black-1;
+    sum_black = R2_black ? sum_black : sum_black-1;
+    sum_black = CT_black ? sum_black : sum_black-1;
+    sum_black = L2_black ? sum_black : sum_black-1;
+    sum_black = L1_black ? sum_black : sum_black-1;
+    Serial.print(L1_black);
+    Serial.print(L2_black);
+    Serial.print(CT_black);
+    Serial.print(R2_black);
+    Serial.print(R1_black);
+    Serial.print(" ");
+    Serial.print(sum_black);
+    Serial.print(" ");
+    Serial.println(location_info);
+
+    if (sum_black <= 2){
+        if (CT_black){
+            MoveS();
+        }
+        else
+        {
+            if ((R1_black || R2_black) && (L1_black || L2_black) ){
+                Move(0, 0);
+            }
+            else
+            {
+                if (R1_black || R2_black)
+                {
+                    MoveR();
+                }
+                else if (L1_black || L2_black) 
+                {
+                    MoveL();
+                }
+                else 
+                {
+                    MoveS();
+                }
+            }
+        }
+        flag = true;
+    }
+    else
+    {
+        Serial.print("else=================================");
+        Serial.println(flag);
+        if (flag)
+        {
+            Move(0, 0);
+            delay(5);
+            location_info += 1; 
+            flag = false;
+        }
+
+    }
+
+}
+void MoveR(){
+    Serial.println("RIGHT");
     Move(MSL, 0);
     delayMicroseconds(10);
-  }
-  else if (analogRead(L1) < DL1) {
+
+}
+void MoveL(){
+    Serial.println("LEFT");
     Move(0, MSR);
     delayMicroseconds(10);
-  }
-  else { //fwdstr
+}
+void MoveS(){
+    Serial.println("Straight");
     Move(MSL, MSR);
-    delayMicroseconds(50);
-  }
+    delayMicroseconds(10);
 }
 
 void Move (int Lp, int Rp) {
-  if (Lp >= 0 && Rp >= 0) {
-    analogWrite(LF, Lp);
-    analogWrite(LB, 0);
-    analogWrite(RF, Rp);
-    analogWrite(RB, 0);
-  }
-  else if (Lp < 0 && Rp > 0) {
-    analogWrite(LF, 0);
-    analogWrite(LB, -Lp);
-    analogWrite(RF, Rp);
-    analogWrite(RB, 0);
-  }
-  else if (Lp > 0 && Rp < 0) {
-    analogWrite(LF, Lp);
-    analogWrite(LB, 0);
-    analogWrite(RF, 0);
-    analogWrite(RB, -Rp);
-  }
-  else {
-    analogWrite(LF, 0);
-    analogWrite(LB, -Lp);
-    analogWrite(RF, 0);
-    analogWrite(RB, -Rp);
-  }
+    if (Lp >= 0 && Rp >= 0) {
+        analogWrite(LF, Lp);
+        analogWrite(LB, 0);
+        analogWrite(RF, Rp);
+        analogWrite(RB, 0);
+    }
+    else if (Lp < 0 && Rp > 0) {
+        analogWrite(LF, 0);
+        analogWrite(LB, -Lp);
+        analogWrite(RF, Rp);
+        analogWrite(RB, 0);
+    }
+    else if (Lp > 0 && Rp < 0) {
+        analogWrite(LF, Lp);
+        analogWrite(LB, 0);
+        analogWrite(RF, 0);
+        analogWrite(RB, -Rp);
+    }
+    else {
+        analogWrite(LF, 0);
+        analogWrite(LB, -Lp);
+        analogWrite(RF, 0);
+        analogWrite(RB, -Rp);
+    }
 }
 
 /*void Move (int Lp, int Rp) {
   if (Lp >= 0) {
     analogWrite(LF, Lp);
     analogWrite(LB, 0);
-  }
+  }
   else {
     analogWrite(LF, 0);
     analogWrite(LB, -Lp);
