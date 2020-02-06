8a6f906d1450f78296c00c693cd90e6a7e666805
NanokiTakahashi <takahashinaoki521@gmail.com>
2020-02-06 16:53:44 +0900

    実験のlinetrace

--- master_main/Main_Program/Main_Program.ino
+++ master_main/Main_Program/Main_Program.ino
@@ -1,4 +1,4 @@
-#include"parameters.h"
+#include "parameters.h"
 
 /*
   Mode and zones
@@ -11,138 +11,135 @@
   mode 7: A→Goal
 */
 
-void setup() {
-  pinMode(RF, OUTPUT);
-  pinMode(RB, OUTPUT);
-  pinMode(LF, OUTPUT);
-  pinMode(LB, OUTPUT);
-  for (i = 0; i < 8; i++) {
-    pinMode(ModeSW[i], INPUT_PULLUP);
-  }
-  location_info = SWCheck();
-  Serial.begin(9600);//PC (Debug)
-  Serial1.begin(9600);//ESP (Wireless)
-  Serial2.begin(9600);//sub Arduino (Timer)
-  Serial2.write("1");
-  Serial.print("=================================================================");
-  Serial.println(location_info);
+void setup()
+{
+    pinMode(RF, OUTPUT);
+    pinMode(RB, OUTPUT);
+    pinMode(LF, OUTPUT);
+    pinMode(LB, OUTPUT);
+    for (i = 0; i < 8; i++)
+    {
+        pinMode(ModeSW[i], INPUT_PULLUP);
+    }
+    location_info = SWCheck();
+    Serial.begin(9600);  //PC (Debug)
+    Serial1.begin(9600); //ESP (Wireless)
+    Serial2.begin(9600); //sub Arduino (Timer)
+    Serial2.write("1");
+    Serial.print("=================================================================");
+    Serial.println(location_info);
 }
 
-void loop() {
-  int sum_black = 5;
-  const bool R1_black = analogRead(R1) < DR1;
-  const bool R2_black = analogRead(R2) < DR2;
-  const bool CT_black = analogRead(CT) < DCT;
-  const bool L1_black = analogRead(L2) < DL2;
-  const bool L2_black = analogRead(L1) < DL1;
-  sum_black = R1_black ? sum_black : sum_black - 1;
-  sum_black = R2_black ? sum_black : sum_black - 1;
-  sum_black = CT_black ? sum_black : sum_black - 1;
-  sum_black = L2_black ? sum_black : sum_black - 1;
-  sum_black = L1_black ? sum_black : sum_black - 1;
-  Serial.print(L1_black);
-  Serial.print(L2_black);
-  Serial.print(CT_black);
-  Serial.print(R2_black);
-  Serial.print(R1_black);
-  Serial.print(" ");
-  Serial.print(sum_black);
-  Serial.print(" ");
-  Serial.println(location_info);
+void loop()
+{
+    int sum_black = 5;
+    const bool R1_black = analogRead(R1) < DR1;
+    const bool R2_black = analogRead(R2) < DR2;
+    const bool CT_black = analogRead(CT) < DCT;
+    const bool L1_black = analogRead(L2) < DL2;
+    const bool L2_black = analogRead(L1) < DL1;
+    sum_black = R1_black ? sum_black : sum_black - 1;
+    sum_black = R2_black ? sum_black : sum_black - 1;
+    sum_black = CT_black ? sum_black : sum_black - 1;
+    sum_black = L2_black ? sum_black : sum_black - 1;
+    sum_black = L1_black ? sum_black : sum_black - 1;
+    Serial.print(L1_black);
+    Serial.print(L2_black);
+    Serial.print(CT_black);
+    Serial.print(R2_black);
+    Serial.print(R1_black);
+    Serial.print(" ");
+    Serial.print(sum_black);
+    Serial.print(" ");
+    Serial.println(location_info);
 
-  if (sum_black <= 2) {
-    if (CT_black) {
-      MoveS();
+    if (sum_black <= 2)
+    {
+        if (CT_black)
+        {
+            MoveStraight();
+        }
+        else
+        {
+            if ((R1_black || R2_black) && (L1_black || L2_black))
+            {
+                MoveStop(1);
+            }
+            else
+            {
+                if (R1_black || R2_black)
+                {
+                    MoveSpinR();
+                }
+                else if (L1_black || L2_black)
+                {
+                    MoveSpinL();
+                }
+                else
+                {
+                    MoveStraight();
+                }
+            }
+        }
+        flag = true;
     }
-    else {
-      if ((R1_black || R2_black) && (L1_black || L2_black) ) {
-        Move(0, 0);
-      }
-      else {
-        if (R1_black || R2_black) {
-          MoveRS();
+    else
+    {
+        Serial.print("==================================================================");
+        Serial.println(flag);
+        switch (location_info)
+        {
+        // start line
+        case 0:
+            Serial.print("StartLine passed");
+        // point A
+        case 1:
+            MoveStop(3000);
+            break;
+        //  point B
+        case 2:
+            //左右に揺れる
+            MoveSpinL();
+            delay(500);
+            MoveSpinR();
+            delay(1000);
+            MoveSpinL();
+            delay(1000);
+            MoveSpinR();
+            delay(1000);
+            // 黒になるまで旋回
+            while (analogRead(CT) > DCT)
+            {
+                MoveL();
+            }
+            Serial.println("Line Ditected");
+            break;
+        // point C path
+        case 3:
+            MoveL_UntillB();
+            break;
+        // point C
+        case 4:
+            MoveStraight();
+            delay(500);
+            MoveL_UntillB();
+            break;
+        // point C path again
+        case 5:
+            MoveL_UntillB();
+            MoveL_UntillB();
+            break;
+        // point A again
+        case 6:
+            MoveStop(3000);
+            break;
+        default:
+            MoveStop(3000);
         }
-        else if (L1_black || L2_black) {
-          MoveLS();
-        }
-        else {
-          MoveS();
-        }
-      }
     }
-    flag = true;
-  }
-  else {
-    Serial.print("==================================================================");
-    Serial.println(flag);
-    switch (location_info) {
-      // start line
-      case 0:
-      // point A
-      case 1:
-        MoveStop(5000);
-        break;
-      //  point B
-      case 2:
-        // すこし前進して一回転
-        /*MoveS();
-        delay(1500);
-        while (analogRead(CT) > DCT) {
-          MoveLS();
-        }
-        Move90L();
-        while (analogRead(CT) > DCT) {
-          MoveLS();
-        }
-        Move90L();*/
-        //左右に揺れる
-        MoveLS();
-        delay(500);
-        MoveRS();
-        delay(1000);
-        MoveLS();
-        delay(1000);
-        MoveRS();
-        delay(1000);
-        Move90L();
-        break;
-      // point C path
-      case 3:
-        while (analogRead(CT) > DCT) {
-          MoveL();
-          delay(500);
-        }
-        Move90L();
-        break;
-      // point C
-      case 4:
-        MoveS();
-        delay(500);
-        while (analogRead(CT) > DCT) {
-          MoveL();
-          delay(500);
-        }
-        Move90L();
-        break;
-      // point C path again
-      case 5:
-        Move90L();
-        while (analogRead(CT) > DCT) {
-          MoveL();
-          delay(500);
-        }
-        Move90L();
-        break;
-      // point A again
-      case 6:
-        MoveStop(5000);
-        break;
-      default:
-        MoveStop(5000);
+    if (flag)
+    {
+        location_info += 1;
+        flag = false;
     }
-    if (flag) {
-      location_info += 1;
-      flag = false;
-    }
-  }
 }
--- master_main/Main_Program/functions.ino
+++ master_main/Main_Program/functions.ino
@@ -1,84 +1,106 @@
-#include"parameters.h"
+#include "parameters.h"
 
-void MoveStop(int time) {
+void MoveStop(int time)
+{
   Serial.println("90 LEFT");
   Move(0, 0);
   delay(time);
 }
-void Move90L() {
-  while (true) {
-    MoveLS();
-    if (analogRead(CT) < DCT) {
-      break;
-    }
-    Serial.println("Line Ditected");
+void MoveL_UntillB()
+{
+  // 白になるまで旋回
+  while (analogRead(CT) < DCT)
+  {
+    MoveL();
   }
+  // 黒になるまで旋回
+  while (analogRead(CT) > DCT)
+  {
+    MoveL();
+  }
+  Serial.println("Line Ditected");
 }
-void MoveRS() {
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+// 右旋回
+void MoveSpinR()
+{
   Serial.println("RIGHT SPIN");
   Move(MSL, -MSR);
-  delayMicroseconds(10);
 }
-void MoveR() {
+void MoveR()
+{
   Serial.println("RIGHT");
   Move(MSL, 0);
-  delayMicroseconds(10);
-  //Move(MSL,-MSR);
 }
-void MoveLS() {
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+// 左旋回
+void MoveSpinL()
+{
   Serial.println("LEFT SPIN");
   Move(-MSL, MSR);
-  delayMicroseconds(10);
 }
-void MoveL() {
+void MoveL()
+{
   Serial.println("LEFT");
   Move(0, MSR);
-  delayMicroseconds(10);
-  //Move(-MSL,MSR);  
 }
-void MoveS() {
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+// 前進
+void MoveStraight()
+{
   Serial.println("Straight");
   Move(MSL, MSR);
-  delayMicroseconds(10);
 }
 
-void Move (int Lp, int Rp) {
-  if (Lp >= 0) {
+void Move(int Lp, int Rp)
+{
+  if (Lp >= 0)
+  {
     analogWrite(LF, Lp);
     analogWrite(LB, 0);
   }
-  else {
+  else
+  {
     analogWrite(LF, 0);
     analogWrite(LB, -Lp);
   }
-  if (Rp >= 0) {
+  if (Rp >= 0)
+  {
     analogWrite(RF, Rp);
     analogWrite(RB, 0);
   }
-  else {
+  else
+  {
     analogWrite(RF, 0);
     analogWrite(RB, -Rp);
   }
 }
 
-int SWCheck() {
-  for (i = 0; i < 8; i++) {
-    if (digitalRead(ModeSW[i]) == LOW) {
-      return i;
-    }
-  }
-}
-
-int RemoteControl() {
+int RemoteControl()
+{
   //Not ready yet
   location_info++;
 }
 
-int TimeAdjust() {
+int TimeAdjust()
+{
   Serial.print("2");
-  while (true) {
-    if (Serial2.read() == 1) {
+  while (true)
+  {
+    if (Serial2.read() == 1)
+    {
       break;
+    }
+  }
+}
+
+int SWCheck()
+{
+  for (i = 0; i < 8; i++)
+  {
+    if (digitalRead(ModeSW[i]) == LOW)
+    {
+      return i;
     }
   }
 }
